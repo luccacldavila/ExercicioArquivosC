89b757beb3698466df9501fe5d0f317214f502c2
Lucca Costa Lima D'Avila <54512319+luccacldavila@users.noreply.github.com>
2025-05-20 17:50:21 -0300

    Add files via upload
    Em produção...

---
+++ ProjetoJogoAritimetico/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+
+int valor1;
+int valor2;
+int resultado;
+int operacao;
+int dificuldade;
+
+} Calcular;
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
+
+void jogar(){
+//Será implementado
+}
+
+void Mostrarinfo()
+{
+    //Sera implementado
+}
